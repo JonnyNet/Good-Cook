28104480f0becc0f2037925dabf0c6210883e875
Jonny Rojas <jrr1047@gmail.com>
2014-10-23 18:24:19 -0500

    Vista datos de empresa lista

--- src/com/example/good/DatosEmpresa.java
+++ src/com/example/good/DatosEmpresa.java
@@ -3,11 +3,11 @@ package com.example.good;
 import android.app.Activity;
 import android.os.Bundle;
 
-public class AdminSeciones extends Activity {
+public class DatosEmpresa extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.admin_seciones);
+		setContentView(R.layout.datos_empresa);
 	}
 }
